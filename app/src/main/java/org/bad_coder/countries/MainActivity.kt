8ebf00a3dd1f883306cdbc4ac84b3fb696d18c3a
Md. Mahfujur Rahman Arif <arif.lu.cse@gmail.com>
2023-01-17 21:59:14 +0100

    Create an empty project

---
+++ app/src/main/java/org/bad_coder/countries/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.bad_coder.countries
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
